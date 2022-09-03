10ee6841d24f47ac49e3e30ad6ecfacfab0aafda
junaid shahid <junaid.shahid.1@gmail.com>
2022-09-03 16:00:33 +0500

    C# WPF Tutorial – RoutedCommand

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace JWPF
 {
@@ -23,6 +11,62 @@ namespace JWPF
         public MainWindow()
         {
             InitializeComponent();
+
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.New, NewCommandExecuted, AllowNew));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Open, OpenCommandExecuted, AllowOpen));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Save, SaveCommandExecuted, AllowSave));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Properties, PropertiesCommandExecuted, AllowProperties));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Print, PrintCommandExecuted, AllowPrint));
+
+        }
+
+        private void PrintCommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("You want to print.");
+        }
+
+        private void AllowPrint(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void PropertiesCommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("You want to open properties.");
+        }
+
+        private void AllowProperties(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void NewCommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("You want to create new file.");
+        }
+
+        private void AllowNew(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void OpenCommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("You want to open existing file.");
+        }
+
+        private void AllowOpen(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void SaveCommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("You want to save a file.");
+        }
+        private void AllowSave(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
         }
     }
 }
